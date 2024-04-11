d08d6bdf390fac65316d21896f4d6c34ccf363e6
nemooo-trash <bam2000000@gmail.com>
2024-04-12 00:26:38 +0300

    add api address

--- Graduation_Work_With_Django/Incidents/static/map.js
+++ Graduation_Work_With_Django/Incidents/static/map.js
@@ -11,9 +11,10 @@ function create_map(center, zoom){
         attribution:  '©OpenRailwayMap'
     }).addTo(map);
 
-    var jsonData = JSON.parse(document.querySelector('#jsonData').getAttribute('data-json'));
-    console.log(jsonData);
-    var description = jsonData.map((item) => item.description);
+    if (document.getElementById('#jsonData') != null){
+        var jsonData = JSON.parse(document.querySelector('#jsonData').getAttribute('data-json'));
+        var description = jsonData.map((item) => item.description);
+    }
 
         var redIcon = new L.Icon({
           iconUrl: '/static/images/marker-icon-2x-red.png',
@@ -40,6 +41,7 @@ function create_map(center, zoom){
           shadowSize: [41, 41]
     });
 
+    if (document.getElementById('#jsonData') != null){
     for (let i = 0; i < jsonData.length; i++) {
 
         switch (jsonData[i].specification__color) {
@@ -59,16 +61,8 @@ function create_map(center, zoom){
         marker.bindPopup(jsonData[i].description.toString()).openPopup();
         marker.addTo(map);
     }
+    }
 
-
-
-
-
-    //L.marker([55.7429063961456, 37.662975621024664], {icon: redIcon}).addTo(map);
-
-//    var marker = new L.Marker(center);
-//    marker.bindPopup(description.toString()).openPopup();
-//    marker.addTo(map);
 
        var featureGroup = L.featureGroup([railwayLayer, baseMap]);
        featureGroup.addTo(map);
